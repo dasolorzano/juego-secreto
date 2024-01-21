2a3d664fead4f33b5fe60f47adce5ee14002652e
dasolorzano <119319898+dasolorzano@users.noreply.github.com>
2024-01-21 12:22:46 -0600

    Update app.js
    Functions final version

--- app.js
+++ app.js
@@ -1,61 +1,72 @@
-// Genera un número secreto al cargar la página
-let numeroSecreto = 0 ;
+// declaraciones
+let numeroSecreto = 0;
 let intentos = 0;
-// Función para asignar texto a un elemento HTML
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
+let listaNumerosSorteados = [];
+let numeroMaxico = 10;
+
+console.log(numeroSecreto);
 
 // Función para verificar el intento del usuario
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
     if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p', `¡Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
-    document.getElementById('reiniciar').removeAttribute('disabled'); 
+        document.querySelector('p').innerHTML = `¡Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`;
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
-        //el usuario no acerto
+        // el usuario no acertó
         if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p', 'El número secreto es menor');
+            document.querySelector('p').innerHTML = 'El número secreto es menor';
         } else {
-            asignarTextoElemento('p', 'El número secreto es mayor');
+            document.querySelector('p').innerHTML = 'El número secreto es mayor';
         }
         intentos++;
         limpiarCaja();
     }
-    // Quita el siguiente return, ya que no es necesario
-}
-function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = '';
-//let valoreCaja = document.querySelector('#valorUsuario');
-//valoreCaja.value = '';
+    // Remove return, as it is unnecessary
 }
 
-// Función para generar un número secreto aleatorio entre 1 y 10
-function generaNumeroSecreto() {
-    return Math.floor(Math.random() * 10) + 1;
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
 }
+
+// Función para generar un número secreto aleatorio entre 1 y numeroMaxico
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random() * numeroMaxico) + 1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //si ya sorteo todos los numeros
+    if (listaNumerosSorteados.length === numeroMaxico) {
+        document.querySelector('p').innerHTML = 'Ya se sortearon todos los números posibles';
+    } else {
+        //si el numero generada esta en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
 function condicionesIniciales() {
     // Asigna texto a elementos HTML al cargar la página
-    asignarTextoElemento('h1', 'Juego del número secreto!!');
-    asignarTextoElemento('p', 'Ingresa un número del 1 al 10');   
-    numeroSecreto = generaNumeroSecreto();
+    document.querySelector('h1').innerHTML = 'Juego del número secreto!!';
+    document.querySelector('p').innerHTML = `Ingresa un número del 1 al ${numeroMaxico}`;
+    numeroSecreto = generarNumeroSecreto();
     intentos = 1;
 }
 
 function reiniciarJuego() {
     //limpiar caja
     limpiarCaja();
-    //indicar menssaje de intervalo de numeros 
-    //general numero aleatorio
-    //inicialisar numero de intentos
-    condicionesIniciales ();
-    //desabilitar el boton juego nuevo
-    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    //indicar mensaje de intervalo de numeros 
+    //generar numero aleatorio
+    //inicializar numero de intentos
+    condicionesIniciales();
+    //deshabilitar el boton juego nuevo
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
 }
-    
 
-
-condicionesIniciales ();
+condicionesIniciales();
